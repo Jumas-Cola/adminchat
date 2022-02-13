d67c35fe89adcd54b0241f3f0be23978d805c61d
Jumas Cola <kbbyfl91@gmail.com>
2022-02-13 20:33:39 +0300

    add app.blade.php layout

--- resources/views/footer.blade.php
+++
@@ -1,3 +0,0 @@
-<script type="text/javascript" src="/js/app.js"></script>
-<script src="/vendor/laravel-admin-ext/adminchat/js/app.js"></script>
-<link rel="stylesheet" href="/vendor/laravel-admin-ext/adminchat/css/app.css">
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Admin Chat</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/vendor/laravel-admin-ext/adminchat/css/app.css">
+</head>
+
+<body>
+  @yield('content')
+</body>
+
+<script type="text/javascript" src="/js/app.js"></script>
+<script src="/vendor/laravel-admin-ext/adminchat/js/app.js"></script>
+</html> 
