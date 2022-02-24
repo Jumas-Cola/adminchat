0d5b93ba9a7b090e641c7e59981127574fc2ab19
Jumas Cola <kbbyfl91@gmail.com>
2022-02-24 18:16:00 +0300

    remove html tag from chat layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,16 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <title>Admin Chat</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="/vendor/laravel-admin-ext/adminchat/css/app.css">
-</head>
+<link rel="stylesheet" href="/vendor/laravel-admin-ext/adminchat/css/app.css">
 
-<body>
+<div>
   @yield('content')
-</body>
+</div>
 
 <script type="text/javascript" src="/js/app.js"></script>
 <script src="/vendor/laravel-admin-ext/adminchat/js/app.js"></script>
-</html> 
