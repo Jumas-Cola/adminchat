403b36ecd48fc460b9b801b1fcee2ed8f4c5ab62
Jumas <kbbyfl91@gmail.com>
2022-08-19 21:59:21 +0300

    upd api route group

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,15 @@
 use JumasCola\AdminChat\AdminChat;
 use JumasCola\AdminChat\Http\Controllers\AdminChatMessageController;
 
-$auth = AdminChat::config('auth_middleware', 'auth:api');
+$auth = AdminChat::config("auth_middleware", "auth:api");
 
-Route::middleware($auth)->group(function () use ($auth) { 
-    Route::get('adminchat/messages', AdminChatMessageController::class.'@index')
-        ->middleware($auth);
-    Route::post('adminchat/messages', AdminChatMessageController::class.'@store')
-        ->middleware($auth);
+Route::middleware($auth)->group(function () use ($auth) {
+    Route::get(
+        "adminchat/messages",
+        AdminChatMessageController::class . "@index"
+    );
+    Route::post(
+        "adminchat/messages",
+        AdminChatMessageController::class . "@store"
+    );
 });
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,8 @@
 
 use JumasCola\AdminChat\Http\Controllers\AdminChatController;
 
-Route::get('adminchat/ajax/search', AdminChatController::class.'@search');
-Route::get('adminchat/ajax/{user}', AdminChatController::class.'@show_api');
-Route::get('adminchat/{user}', AdminChatController::class.'@show');
-Route::get('adminchat', AdminChatController::class.'@index');
-Route::post('adminchat/{user}', AdminChatController::class.'@store');
+Route::get("adminchat/ajax/search", AdminChatController::class . "@search");
+Route::get("adminchat/ajax/{user}", AdminChatController::class . "@show_api");
+Route::get("adminchat/{user}", AdminChatController::class . "@show");
+Route::get("adminchat", AdminChatController::class . "@index");
+Route::post("adminchat/{user}", AdminChatController::class . "@store");
